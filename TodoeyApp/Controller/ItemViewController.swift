b6f0d8d66a02a7d5cfbc63cf2026070043907d79
Battal Ucar <battalucar@outlook.com>
2022-10-30 12:58:01 +0300

    initial commit

---
+++ TodoeyApp/Controller/ItemViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TodoeyApp
+//
+//  Created by Battal Uçar on 30.10.2022.
+//
+
+import UIKit
+
+class ItemViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
