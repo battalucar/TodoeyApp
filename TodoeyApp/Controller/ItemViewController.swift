7b4134b2c878695dc16af83633cb258e4f2f8ec9
Battal Ucar <battalucar@outlook.com>
2022-10-30 15:00:40 +0300

    feat: Add todos and display todos on screen.

--- TodoeyApp/Controller/ItemViewController.swift
+++ TodoeyApp/Controller/ItemViewController.swift
@@ -8,12 +8,74 @@
 import UIKit
 
 class ItemViewController: UITableViewController {
-
+    
+    var itemArray = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
+    
+    // MARK: - Tableview data source
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ItemCell", for: indexPath)
+        
+        if #available(iOS 14.0, *) {
+            var content = cell.defaultContentConfiguration()
+            content.text = itemArray[indexPath.row]
+            cell.contentConfiguration = content
+        } else {
+            cell.textLabel?.text = itemArray[indexPath.row]
+        }
+        
+        return cell
+    }
+    
+    // MARK: - Tableview delegate
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    // MARK: - Add new Item
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add New Item", message: "", preferredStyle: .alert)
+        
+        let addAction = UIAlertAction(title: "Add", style: .default) { action in
+            self.itemArray.append(textField.text!)
+            self.tableView.reloadData()
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+        
+        alert.addTextField { alertTextField in
+            alertTextField.placeholder = "Create new item"
+            textField = alertTextField
+        }
+        
+        alert.addAction(cancelAction)
+        alert.addAction(addAction)
+        
+        self.present(alert, animated: true, completion: nil)
+    }
+    
 }
 
---
+++ TodoeyApp/Model/Item.swift
@@ -0,0 +1,13 @@
+//
+//  Item.swift
+//  TodoeyApp
+//
+//  Created by Battal Uçar on 30.10.2022.
+//
+
+import Foundation
+
+class Item: Decodable {
+    var title: String = ""
+    var done: Bool = false
+}
